7cd3d94ab96ed8bc93f596957793f0ac4a176624
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2018-11-22 16:07:58 +0530

    update

---
+++ arduino_logic_tester.ino
@@ -0,0 +1,56 @@
+int a,b,c,d,e,f,g,h;
+//declaring OUPUT pin variables.
+void setup(){
+	Serial.begin(115200);
+	Serial.println("Arduino Based Digital Logic Tester:");
+	Serial.println("Choose Logic Gate:");
+	Serial.println("\t1. AND \n\t2. OR \n\t3. NOT \n\t4. NAND \n\t5. NOR \n\t6. XOR");
+}
+void loop(){
+	if(Serial.available()>0){
+		input=Serial.read(DEC);
+	}
+}
+void setmode(){
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+}
+void andgate(){
+	
+}
+void notgate(){
+	
+}
+void orgate(){
+	
+}
+void nandgate(){
+	
+}
+void norgate(){
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+	pinMode(,OUPUT);
+	pinMode(,OUPUT);
+	pinMode(,INPUT);
+}
+void xorgate(){
+	
+}
